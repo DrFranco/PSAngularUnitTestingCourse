978d227f4ac6393b779e73e3786fe96c58da04a1
Michael Franco <mikedfranco@gmail.com>
2022-08-16 12:06:33 -0700

    async testing and code coverage

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { ComponentFixture, fakeAsync, flush, TestBed, tick, waitForAsync } from "@angular/core/testing"
 import { ActivatedRoute } from "@angular/router";
 import { HeroService } from "../hero.service";
 import { HeroDetailComponent } from "./hero-detail.component"
@@ -35,5 +35,25 @@ describe('HeroDetailComponent', () => {
         fixture.detectChanges();
 
         expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERDUDE');
-    })
+    });
+
+    it('should call updateHero when save is called', fakeAsync(() => {
+        mockHeroService.updateHero.and.returnValue(of({}));
+        fixture.detectChanges();
+
+        fixture.componentInstance.save();
+        flush();
+
+        expect(mockHeroService.updateHero).toHaveBeenCalled();
+    }))
+//     it('should call updateHero when save is called', waitForAsync(() => {
+//         mockHeroService.updateHero.and.returnValue(of({}));
+//         fixture.detectChanges();
+
+//         fixture.componentInstance.save();
+
+//         fixture.whenStable().then(() => {
+//             expect(mockHeroService.updateHero).toHaveBeenCalled();
+//         });
+//     }))
 })
